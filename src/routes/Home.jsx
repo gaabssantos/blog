a8e612794bc46c584fc723f8ed5ce0289e6ed743
Gabriel Santos <gabriel.oliveira20500@gmail.com>
2023-11-08 15:17:28 -0300

    added the axios get

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,42 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+import { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default Home
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+      const data = res.data;
+
+      setPosts(data);
+    } catch (err) {
+      console.log(`Houve um erro: ${err}`);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando...</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">Ler mais</Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Home;
